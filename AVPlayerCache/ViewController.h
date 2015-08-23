2c65093606c953dd4f241951539be0c53ec4aadc
Dmitry Kopanitsa <dmitry.kopanitsa@icloud.com>
2015-08-23 16:36:44 +0300

    Initial Commit

---
+++ AVPlayerCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayerCache
+//
+//  Created by Dmitry on 23.08.15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVPlayerCache/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVPlayerCache
+//
+//  Created by Dmitry on 23.08.15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
